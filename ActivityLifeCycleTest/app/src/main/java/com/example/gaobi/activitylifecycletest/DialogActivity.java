759912154a7d9e37fd0900252e9621d1612cf587
Gavin <gaobin555@163.com>
2018-11-24 10:23:35 +0800

    First commit
    Signed-off-by: Gavin <gaobin555@163.com>

---
+++ ActivityLifeCycleTest/app/src/main/java/com/example/gaobi/activitylifecycletest/DialogActivity.java
@@ -0,0 +1,13 @@
+package com.example.gaobi.activitylifecycletest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DialogActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
+    }
+}
