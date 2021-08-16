783573b5afb736a746391c10bf313499ade78a3b
gaob <gaob@x-thinks.com>
2021-08-16 09:35:21 +0800

    update AddTimeWatermark

--- CameraAlbumTest/app/src/main/java/com/example/gavin/cameraalbumtest/BitmapUtil.java
+++ CameraAlbumTest/app/src/main/java/com/example/gavin/cameraalbumtest/BitmapUtil.java
@@ -5,34 +5,56 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 
+import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class BitmapUtil {
     /**
      * 添加时间水印
-     * @param mBitmap
-     * @return mNewBitmap
+     * @param bitmap
+     * @return mewBitmap
      */
-    public static Bitmap AddTimeWatermark(Bitmap mBitmap) {
-        //获取原始图片与水印图片的宽与高
-        int mBitmapWidth = mBitmap.getWidth();
-        int mBitmapHeight = mBitmap.getHeight();
-        Bitmap mNewBitmap = Bitmap.createBitmap(mBitmapWidth, mBitmapHeight, Bitmap.Config.ARGB_8888);
-        Canvas mCanvas = new Canvas(mNewBitmap);
-        //向位图中开始画入MBitmap原始图片
-        mCanvas.drawBitmap(mBitmap,0,0,null);
-        //添加文字
-        Paint mPaint = new Paint();
-        String mFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss EEEE").format(new Date());
-        //String mFormat = TingUtils.getTime()+"\n"+" 纬度:"+GpsService.latitude+"  经度:"+GpsService.longitude;
-        mPaint.setColor(Color.RED);
-        mPaint.setTextSize(30);
-        //水印的位置坐标
-        mCanvas.drawText(mFormat, (mBitmapWidth * 1) / 10,(mBitmapHeight*14)/15,mPaint);
-        mCanvas.save(Canvas.ALL_SAVE_FLAG);
-        mCanvas.restore();
+    public static Bitmap AddTimeWatermark(Bitmap bitmap) {
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+        Bitmap newBitmap = Bitmap.createBitmap(bitmapWidth, bitmapHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(newBitmap);
+        // draw bitmap to new bitmap
+        canvas.drawBitmap(bitmap,0,0,null);
+        // get system time
+        Paint paint = new Paint();
+        String format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss EEEE").format(new Date());
+        paint.setColor(Color.RED);
+        paint.setTextSize(30);
+        // Watermark position coordinates
+        canvas.drawText(format, (bitmapWidth * 1) / 10,(bitmapHeight*14)/15, paint);
+        canvas.save(Canvas.ALL_SAVE_FLAG);
+        canvas.restore();
 
-        return mNewBitmap;
+        return newBitmap;
+    }
+
+    public static byte[] addTimeWatermarkToJpeg(byte[] jpeg) {
+        LogHelper.d(TAG, "[addTimeWatermarkToJpeg] jpeg = " + jpeg);
+        ExifInterface exif = getExif(jpeg);
+        int jpegWidth = getJpegWidth(exif);
+        Size exifSize = CameraUtil.getSizeFromExif(jpegData);
+        Bitmap bitmap = decodeBitmapFromJpeg(jpeg, jpegWidth);
+
+        Bitmap newBitmap = addTimeWatermarkToBitmap(bitmap);
+
+        return getBytesByBitmap(newBitmap);
+    }
+
+    /**
+     * getBytesByBitmap
+     * @author      gaob@x-thinks.com
+     * @version     1.0, 1/5/2019
+     * @param bitmap
+     * */
+    private static byte[] getBytesByBitmap(Bitmap bitmap) {
+        ByteBuffer buffer = ByteBuffer.allocate(bitmap.getByteCount());
+        return buffer.array();
     }
 }
